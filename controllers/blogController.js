6aae96bf2d933a40efbdd677812b1e27425664c4
eagyemang21 <elijahagyemang9@gmail.com>
2022-08-09 13:54:27 -0400

    all posts and users routes working

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,13 +1,14 @@
-const { getAllPostsFromDB,  } = require("../models/postModel")
+const Posts = require("../models/postModel")
+const { getAllPostsFromDB,createPostInDB, displayPostFromDB, deletePostFromDB} = require("../models/postModel")
 
-const getAllBlogs = async(req, res) => {
-    
-    try{
-        const posts = getAllPostsFromDB()
+const getAllBlogs = async (req, res) => {
+
+    try {
+        const posts = Posts.getAllPostsFromDB()
         return res.status(200).json({
             posts
         })
-    }catch(err){
+    } catch (err) {
         return res.status(400).send(err.message)
 
     }
@@ -28,21 +29,39 @@ const createBlog = async (req, res) => {
 
 const retrieveBlog = async (req, res) => {
     const post_id = req.params.id
-    try{
-
-    }catch(err){
-
+    try {
+        const postData = await Posts.displayPostFromDB(post_id)
+        return res.status(200).send({ postData })
+    } catch (err) {
+        return res.status(200).json({
+            message: err.message
+        })
 
     }
 }
 
 
-const updateBlog = async(req, res) => {
+const updateBlog = async (req, res) => {
+
+    try {
+
+    } catch (error) {
+
+    }
 
 }
 
-const deleteBlog = async(req, res) =>{
+const deleteBlog = async (req, res) => {
+    const post_id = req.params.id;
 
+    try {
+        const post = await Posts.deletePostFromDB(post_id)
+        return res.status(200).send({ post })
+    } catch (error) {
+        return res.status(400).json({
+            message: err.message
+        })
+    }
 }
 
 module.exports = {
